f44797a48f886d95e16724f33226afdbac2ec624
Iago Páramo Parada <a22iagopp@iessanclemente.net>
2024-01-18 12:55:06 +0100

    added switch

--- script.js
+++ script.js
@@ -7,6 +7,23 @@ function test(){
             checked=answer[index].value;
         }       
     }
-    
-    alert(checked);
+
+    let correct = document.getElementsByID("correct");
+    let incorrect = document.getElementsByID("incorrect");
+
+    switch (checked) {
+        case 1:
+            incorrect.style.display="unset";
+            correct.style.display="none";
+            break;
+        case 2:
+            correct.style.display="unset";
+            incorrect.style.display="none";
+          break;
+        case 3:
+            incorrect.style.display="unset";
+            correct.style.display="none";
+          break;
+      }
+      
 }
